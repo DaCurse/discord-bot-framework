8a28360cf32e27a7cf6a90a6d08e7e87b9680fa7
DaCurse <dacurse0@gmail.com>
2020-12-17 21:57:37 +0200

    Improved typing on module metadata

--- packages/framework/src/interfaces/metadata/module-metadata.interface.ts
+++ packages/framework/src/interfaces/metadata/module-metadata.interface.ts
@@ -1,6 +1,9 @@
+import { BaseCommand } from '../../base-command';
 import { BaseMetadata } from './base-metadata.interface';
 
+type CommandConstructor = new (...args: any[]) => BaseCommand<any>;
+
 export interface ModuleMetadata extends BaseMetadata {
-  commands?: any[];
+  commands?: CommandConstructor[];
   listeners?: any[];
 }
